18b6e01cbe801ff35667a135f364565b816fdf03
Neivin Mathew <neivin93@gmail.com>
2015-10-14 00:53:50 -0400

    removed some boilerplate code

--- js/options.js
+++ js/options.js
@@ -2,12 +2,10 @@
 // // Saves options to chrome.storage.sync.
 function save_options() {
   var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
   var unit_opt = $('input[name="units"]:checked').val();
   var country_opt = document.getElementById('country').value;
   chrome.storage.sync.set({
     favoriteColor: color,
-    likesColor: likesColor,
     units: unit_opt,
     country: country_opt
   }, function() {
@@ -26,12 +24,10 @@ function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     favoriteColor: 'red',
-    likesColor: true,
     units: 'c',
     country: 'CA'
   }, function(items) {
     document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
     $('input[type="radio"][value="'+items.units+'"]').prop('checked',true);
     document.getElementById('country').value = items.country;
   });
